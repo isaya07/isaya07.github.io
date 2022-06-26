f4f039253cbeb784bd0989f3c70dfb158c9fecaf
isaya07 <isaya07@gmail.com>
2022-06-26 16:54:49 +0200

    plusieur changement

--- assets/GoogleFrame.a7e57df0.js
+++
@@ -1,1 +0,0 @@
-import{o,b as a,d as t}from"./vendor.cde18cd2.js";const s={class:"map-responsive"},n={__name:"GoogleFrame",setup(r){const e="https://www.google.com/maps/embed/v1/place?q=place_id:ChIJW2BTT-Tu9UcRdJq2CFpeSjA&key=AIzaSyBa1Nt9gSpYLW1yZukH6XZKcpZ5a4_S0F0";return(l,p)=>(o(),a("div",s,[t("iframe",{style:{border:"0"},loading:"lazy",allowfullscreen:"",src:e})]))}};export{n as default};
